5f16f1c532d583693a865d761adb43bf16255140
harsh-js <harsh@dqotsolutions.com>
2021-12-21 15:12:21 +0530

    modal

--- components/Cards/Leavetable.js
+++ components/Cards/Leavetable.js
@@ -1,234 +1,157 @@
+/** @format */
+
 import React from "react";
 import PropTypes from "prop-types";
 
-
-import  { useState } from "react";
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 // import ApplyLeave from "components/Modal/ApplyLeave";
 
 import "react-datepicker/dist/react-datepicker.css";
 
-
 import TableDropdown from "components/Dropdowns/TableDropdown.js";
 
-
 export default function Leavetable({ color }) {
-  const [startdate, setStartDate] = useState(new Date());
-  const [showModal, setShowModal] = React.useState(false);
-  return (
-    <>
- 
+	const [startdate, setStartDate] = useState(new Date());
+	const [showModal, setShowModal] = React.useState(false);
+	return (
+		<>
+			<div
+				className={
+					"relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
+					(color === "light" ? "bg-white" : "bg-blueGray-700 text-white")
+				}>
+				<div className='rounded-t mb-0 px-4 py-3 border-0'>
+					<div className='flex flex-wrap items-center'>
+						<div className='relative w-full px-4 max-w-full flex-grow flex-1 flex flex-row'>
+							<h3
+								className={
+									"font-semibold text-lg date-heading " +
+									(color === "light" ? "text-blueGray-700" : "text-white")
+								}>
+								Leave History
+							</h3>
+							<DatePicker
+								selected={startdate}
+								onChange={(date) => setStartDate(date)}
+							/>
+							<button
+								className='bg-lightBlue-500 blue-btn active:bg-lightBlue-300 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150'
+								type='button'
+								onClick={() => setShowModal(true)}>
+								Apply For Leave
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className='block w-full overflow-x-auto'>
+					{/* Projects table */}
+					<table className='items-center w-full bg-transparent border-collapse'>
+						<thead>
+							<tr>
+								<th
+									className={
+										"px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+										(color === "light"
+											? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+											: "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+									}>
+									Leave Type
+								</th>
+								<th
+									className={
+										"px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+										(color === "light"
+											? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+											: "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+									}>
+									Half/Full Day
+								</th>
+								<th
+									className={
+										"px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+										(color === "light"
+											? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+											: "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+									}>
+									Number Of Days
+								</th>
+								<th
+									className={
+										"px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+										(color === "light"
+											? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+											: "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+									}>
+									Duration
+								</th>
+								<th
+									className={
+										"px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+										(color === "light"
+											? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+											: "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+									}>
+									Status
+								</th>
+								<th
+									className={
+										"px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+										(color === "light"
+											? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+											: "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+									}>
+									Action Status
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<th className='border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center'>
+									<span
+										className={
+											"font-bold " +
+											+(color === "light" ? "text-blueGray-600" : "text-white")
+										}>
+										Dec 14,2021
+									</span>
+								</th>
+								<td className='border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+									09:30:00
+								</td>
+								<td className='border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+									20:03:00
+								</td>
+								<td className='border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+									10:33
+								</td>
+								<td className='border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+									No
+								</td>
+								<td className='border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+									No
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 
-      <div
-        className={
-          "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
-          (color === "light" ? "bg-white" : "bg-blueGray-700 text-white")
-        }
-      >
-        <div className="rounded-t mb-0 px-4 py-3 border-0">
-          <div className="flex flex-wrap items-center">
-            <div className="relative w-full px-4 max-w-full flex-grow flex-1 flex flex-row">
-              <h3
-                className={
-                  "font-semibold text-lg date-heading " +
-                  (color === "light" ? "text-blueGray-700" : "text-white")
-                }
-              >
-               Leave History
-              </h3>
-              <DatePicker selected={startdate} onChange={(date) => setStartDate(date)}  />
-              <button
-        className="bg-lightBlue-500 blue-btn active:bg-lightBlue-300 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-      Apply For Leave
-      </button>
-              
-            
-            
-            </div>
-          </div>
-        </div>
-        <div className="block w-full overflow-x-auto">
-          {/* Projects table */}
-          <table className="items-center w-full bg-transparent border-collapse">
-            <thead>
-              <tr>
-                <th
-                  className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
-                  }
-                >
-               Leave Type
-                </th>
-                <th
-                  className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
-                  }
-                >
-                 Half/Full Day
-                </th>
-                <th
-                  className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
-                  }
-                >
-                Number Of Days
-
-                </th>
-                <th
-                  className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
-                  }
-                >
-              Duration
-                </th>
-                <th
-                  className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
-                  }
-                >
-             Status
-                </th>
-                <th
-                  className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
-                  }
-                >
-              Action Status
-                </th>
-               
-                
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-                  
-                  <span
-                    className={
-                      "font-bold " +
-                      +(color === "light" ? "text-blueGray-600" : "text-white")
-                    }
-                  >
-                  Dec 14,2021
-                  </span>
-                </th>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                09:30:00
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                   20:03:00
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                10:33
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                No
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                No
-                </td>
-               
-               
-               
-               
-              </tr>
-              
-            
-            </tbody>
-          </table>
-        </div>
-      </div>
-
-      
-
-      {showModal ? (
-        <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
-   
-
-
-     
-    </>
-  );
+			{showModal ? (
+				<div className='ShareModalOut'>
+					<div className='ShareModal'></div>
+				</div>
+			) : (
+				""
+			)}
+		</>
+	);
 }
 
 Leavetable.defaultProps = {
-  color: "light",
+	color: "light",
 };
 
 Leavetable.propTypes = {
-  color: PropTypes.oneOf(["light", "dark"]),
+	color: PropTypes.oneOf(["light", "dark"]),
 };
