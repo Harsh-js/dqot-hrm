6841120c619665cadbda5693717d6e49dcaed523
harsh-js <harsh@dqotsolutions.com>
2021-12-27 16:18:40 +0530

    new

--- network/misc.js
+++ network/misc.js
@@ -6,19 +6,16 @@ import Joi from "joi";
 import { toast } from "react-toastify";
 axios.defaults.baseURL = `${baseUrl}/user/misc/`;
 
-export const SignUpRoute = async ({ body, checked }) => {
+export const ApplyLeaveRoute = async ({ body, user }) => {
 	try {
-		if (!checked) {
-			return toast.error("Please agree with our policy");
-		}
 		// delete body["otp"];
+		console.log(user);
 		const Schema = Joi.object({
-			name: Joi.string().required(),
-			mobile: Joi.number().required(),
-			email: Joi.string()
-				.email({ tlds: { allow: false } })
-				.required(),
-			designation: Joi.string().required(),
+			from: Joi.object().required(),
+			to: Joi.object().required(),
+			reason: Joi.string().required(),
+			day: Joi.number().required(),
+			type: Joi.string().required(),
 		}).validate(body);
 
 		if (Schema.error) {
@@ -28,107 +25,11 @@ export const SignUpRoute = async ({ body, checked }) => {
 
 		const data = Schema.value;
 
-		const response = await axios.post("otp", data);
-
-		if (response.data.status) {
-			console.log(response.data);
-			toast(response.data.message);
-			return response.data;
-		} else {
-			toast.error(response.data.message);
-			return;
-		}
-	} catch (err) {
-		console.log("Erro: " + err);
-		return err;
-	}
-};
-
-export const SignUpOtpRoute = async ({ body, checked }) => {
-	try {
-		if (!checked) {
-			return toast.error("Please agree with our policy");
-		}
-		// delete body["otp"];
-		const Schema = Joi.object({
-			name: Joi.string().required(),
-			mobile: Joi.number().required(),
-			email: Joi.string()
-				.email({ tlds: { allow: false } })
-				.required(),
-			designation: Joi.string().required(),
-			otp: Joi.number().required(),
-		}).validate(body);
-
-		if (Schema.error) {
-			toast.warning(Schema.error.message);
-			return "Error";
-		}
-
-		const data = Schema.value;
-
-		const response = await axios.post("login", data);
-
-		if (response.data.status) {
-			console.log(response.data);
-			toast(response.data.message);
-			return response.data;
-		} else {
-			toast.error(response.data.message);
-			return;
-		}
-	} catch (err) {
-		console.log("Erro: " + err);
-		return err;
-	}
-};
-
-export const LoginRoute = async ({ body }) => {
-	try {
-		// delete body["otp"];
-		const Schema = Joi.object({
-			mobile: Joi.number().required(),
-		}).validate(body);
-
-		if (Schema.error) {
-			toast.warning(Schema.error.message);
-			return "Error";
-		}
-
-		const { mobile } = Schema.value;
-
-		const response = await axios.get(`otp?mobile=${mobile}`);
-
-		if (response.data.status) {
-			console.log(response.data);
-			toast(response.data.message);
-			return response.data;
-		} else {
-			toast.error(response.data.message);
-			return;
-		}
-	} catch (err) {
-		console.log("Erro: " + err);
-		return err;
-	}
-};
-
-export const LoginOtpRoute = async ({ body }) => {
-	try {
-		// delete body["otp"];
-		const Schema = Joi.object({
-			mobile: Joi.number().required(),
-			otp: Joi.number().required(),
-		}).validate(body);
-
-		if (Schema.error) {
-			toast.warning(Schema.error.message);
-			return "Error";
-		}
-
-		const { mobile, otp } = Schema.value;
-
-		const response = await axios.get(`login?mobile=${mobile}&otp=${otp}`);
+		const response = await axios.post("leave-apply", data, {
+			headers: {
+				authorization: `Bearer ${user.token}`,
+			},
+		});
 
 		if (response.data.status) {
 			console.log(response.data);
--- pages/admin/leavehistory.js
+++ pages/admin/leavehistory.js
@@ -2,19 +2,32 @@
 
 // components
 
+import { baseUrl } from "@/configs/index";
 import withAuth from "@/helpers/pages/AuthRoute";
+import axios from "axios";
 import Leavetable from "components/Cards/Leavetable";
 
 // layout for page
 
 import Admin from "layouts/Admin.js";
+import useSWR from "swr";
 
-function Leavehistory() {
+const fetcher = (url, token) =>
+	axios
+		.get(url, { headers: { authorization: `Bearer ${token}` } })
+		.then((res) => res.data);
+
+function Leavehistory({ user }) {
+	const { data, error } = useSWR(
+		[`${baseUrl}/user/misc/leave-apply`, user.token],
+		fetcher
+	);
+	console.log(data?.data);
 	return (
 		<>
 			<div className='flex flex-wrap mt-4'>
 				<div className='w-full mb-12 px-4'>
-					<Leavetable />
+					<Leavetable user={user} leaveData={data?.data} />
 				</div>
 				{/* <div className="w-full mb-12 px-4">
           <CardTable color="dark" />
